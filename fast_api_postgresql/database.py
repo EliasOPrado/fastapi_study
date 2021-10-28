638f7014bdc59699dba470b8a0e4553f29a7647b
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-10-27 22:13:43 -0300

    Added db conection and models file

---
+++ fast_api_postgresql/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(
+    "postgresql://postgres:anyname@localhost/item_db",
+    echo=True
+    )
+
+Base=declarative_base()
+
+SessionLocal=sessionmaker(bind=engine)
+
